7dde4b50d59345e388d38c896ef9fd0b79916d69
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-02-08 11:21:14 -0300

    incluindo a imagem de logo

--- App.js
+++ App.js
@@ -1,7 +1,16 @@
 /* rnfs escola esse antes de começar  e tire alguma coisas e mude outras */
-import { Button, SafeAreaView, StatusBar, Text, View } from "react-native";
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  View,
+} from "react-native";
 
-import estilos from "./src/stylesheets/estilo";
+import estilos from "./src/stylesheets/estilo"; /*  importando css */
+
+import logo from "./assets/images/logo.png"; /* importando imagem */
 
 export default function App() {
   return (
@@ -9,6 +18,7 @@ export default function App() {
       <StatusBar barStyle="light-content" />
       <SafeAreaView style={estilos.container}>
         <View style={estilos.viewLogo}>
+          <Image source={logo} style={estilos.logo} />
           <Text>Dá Hora Filmes!</Text>
         </View>
         <View style={estilos.viewBotoes}>

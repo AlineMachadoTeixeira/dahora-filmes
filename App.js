e2b05b49a5b1a7f4507a3f9e76b005ecb26e78a5
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-02-08 09:37:55 -0300

    criando o  style={estilos

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 /* rnfs escola esse antes de começar  e tire alguma coisas e mude outras */
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <Text>App</Text>
+      <SafeAreaView style={estilos.container}>
+        <View style={estilos.viewLogo}>
+          <Text>Dá Hora Filmes</Text>
+        </View>
+        <View style={estilos.viewBotoes}>
+          <Button title="Buscar Filmes" />
+          <Button title="Favoritos" />
+        </View>
+        <View style={estilos.viewRodape}>
+          <Button title="Privacidade" />
+          <Button title="Sobre" />
+        </View>
       </SafeAreaView>
     </>
   );

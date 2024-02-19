1f7e957c6e42489e827ed2598b4cb630a7f558ad
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-02-19 12:00:36 -0300

    Fazendo pagina privacidade

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StatusBar } from "react-native";
 
 import Home from "./src/screens/Home";
 import Sobre from "./src/screens/Sobre";
+import Privacidade from "./src/screens/Privacidade";
 
 export default function App() {
   return (
@@ -10,7 +11,9 @@ export default function App() {
       <StatusBar barStyle="light-content" />
 
       {/* <Home /> */}
-      <Sobre />
+      {/* <Sobre /> */}
+
+      <Privacidade />
     </>
   );
 }
--- src/screens/Sobre.js
+++ src/screens/Sobre.js
@@ -1,5 +1,13 @@
 //atalho: rnfs
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Linking,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import SafeContainer from "../components/SafeContainer";
 
 import logoAPI from "../../assets/images/logo-tmdb.png";
@@ -8,38 +16,47 @@ export default function Sobre() {
   return (
     <SafeContainer>
       <View style={estilos.subContainer}>
-        <Text style={estilos.subtitulo}>Sobre o app Dá Horas Filmes</Text>
+        {/* ScrollView para tela rola. Cima e baixo  showsVerticalScrollIndicator={false} para não aparecer a barrinha   */}
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Text style={estilos.subtitulo}>Sobre o app Dá Horas Filmes</Text>
 
-        <Text style={estilos.texto}>
-          O <Text style={estilos.nomeApp}>Dá Horas Filmes</Text> é um aplicativo
-          com a finalidade de permitir a busca por informações sobre filmes
-          existentes na base de dados pública disponibilizada pelo site The
-          Movie Database (TMDb).
-        </Text>
+          <Text style={estilos.texto}>
+            O <Text style={estilos.nomeApp}>Dá Horas Filmes</Text> é um
+            aplicativo com a finalidade de permitir a busca por informações
+            sobre filmes existentes na base de dados pública disponibilizada
+            pelo site The Movie Database (TMDb).
+          </Text>
 
-        <View style={estilos.viewApi}>
-          <Pressable></Pressable>
-          <Image
-            /*  resizeMode="contain" é para não corta a imagem, pois não é uadrada*/
-            resizeMode="contain"
-            source={logoAPI}
-            style={estilos.imagemApi}
-          />
-        </View>
+          <View style={estilos.viewApi}>
+            {/* onPress faz uma função para fazer a imagem ficar com link de navegação */}
+            <Pressable
+              onPress={() =>
+                Linking.openURL("https://www.themoviedb.org/?language=pt-BR")
+              }
+            >
+              <Image
+                /*  resizeMode="contain" é para não corta a imagem, pois não é uadrada*/
+                resizeMode="contain"
+                source={logoAPI}
+                style={estilos.imagemApi}
+              />
+            </Pressable>
+          </View>
 
-        <Text style={estilos.texto}>
-          Ao localizar um filme, o usuário pode visualizar informações como
-          título, data de lançamento, nota média de avaliação e uma breve
-          descrição sobre o filme e, caso queira, salvar estas informações em
-          uma lista no próprio aplicativo para visualização posterior.
-        </Text>
+          <Text style={estilos.texto}>
+            Ao localizar um filme, o usuário pode visualizar informações como
+            título, data de lançamento, nota média de avaliação e uma breve
+            descrição sobre o filme e, caso queira, salvar estas informações em
+            uma lista no próprio aplicativo para visualização posterior.
+          </Text>
 
-        <Text style={estilos.texto}>
-          O aplicativo poderá receber novos recursos conforme o feedback e
-          demanda dos usuários.
-        </Text>
+          <Text style={estilos.texto}>
+            O aplicativo poderá receber novos recursos conforme o feedback e
+            demanda dos usuários.
+          </Text>
 
-        <Text style={estilos.texto}>Dá Horas Filmes &copy; 2024</Text>
+          <Text style={estilos.texto}>Dá Horas Filmes &copy; 2024</Text>
+        </ScrollView>
       </View>
     </SafeContainer>
   );

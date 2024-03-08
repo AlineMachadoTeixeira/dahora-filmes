249a8207dda089e9a8943493b027b42b750021a4
Aline Teixeira <131314663+AlineMachadoTeixeira@users.noreply.github.com>
2024-03-08 09:19:02 -0300

    usando keyExtractor

--- src/screens/Resultados.js
+++ src/screens/Resultados.js
@@ -1,5 +1,5 @@
 //atalho: rnfs
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import SafeContainer from "../components/SafeContainer";
 import { api, apiKey } from "../services/api-moviedb";
 import { useEffect, useState } from "react";
@@ -24,7 +24,8 @@ export default function Resultados({ route }) {
             api_key: apiKey,
           },
         });
-        console.log(resposta.data);
+        /* Adicionando os resultados ao state */
+        setResultados(resposta.data.results);
       } catch (error) {
         console.error("Deu ruim: " + error.message);
       }
@@ -36,6 +37,18 @@ export default function Resultados({ route }) {
     <SafeContainer>
       <View style={estilos.subContainer}>
         <Text style={estilos.texto}>Você buscou por: {filme}</Text>
+
+        <View style={estilos.viewFilmes}></View>
+        <FlatList
+          //Prop data apontando para state contendo os dados para FlatList
+          data={resultados}
+          //Extraindo a chave/key de cada registro/item/filme único
+          keyExtractor={(item) => item.id}
+          //Prop que irá renderizar cada item/filme em um camponente
+          renderItem={({ item }) => {
+            return <Text>{item.title}</Text>;
+          }}
+        />
       </View>
     </SafeContainer>
   );
@@ -45,9 +58,14 @@ const estilos = StyleSheet.create({
   subContainer: {
     flex: 1,
     padding: 16,
+    width: "100%",
   },
 
   texto: {
+    marginVertical: 8,
+  },
+
+  viewFilmes: {
     marginVertical: 8,
   },
 });
